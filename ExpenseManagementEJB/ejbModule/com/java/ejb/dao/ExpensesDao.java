e0b353cda502f0a8d5632757f74519631db6b665
Ananta Swain <135028500+Ananta2580@users.noreply.github.com>
2025-06-04 15:58:44 +0530

    Delete ExpenseManagementEJB directory

--- ExpenseManagementEJB/ejbModule/com/java/ejb/dao/ExpensesDao.java
+++
@@ -1,9 +0,0 @@
-package com.java.ejb.dao;
-
-import java.sql.SQLException;
-
-import com.java.ejb.model.Expenses;
-
-public interface ExpensesDao {
-	String addExpensesDao(Expenses expenses) throws ClassNotFoundException, SQLException;
-}
